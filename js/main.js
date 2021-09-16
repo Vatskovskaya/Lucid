07628f2c8f99234d17a3053dd46f002ada466493
Vatskovskaya <86354815+Vatskovskaya@users.noreply.github.com>
2021-09-16 20:22:02 +0300

    finish layout

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
   $('.pricings__slider').slick({
-  arrows: false,
-  dots: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  infinite: true
+    adaptiveHeight: true,
+    arrows: false,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    infinite: true,
+    responsive: [{
+      breakpoint: 940,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }]
   });
 });
